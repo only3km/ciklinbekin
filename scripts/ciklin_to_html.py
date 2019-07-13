8ca8338765a38d4c09b5f56d091e61f81f42ff4c
Radium Zheng <ztl8702@users.noreply.github.com>
2019-07-14 07:06:10 +1000

    Fix

--- scripts/ciklin_to_html.py
+++ scripts/ciklin_to_html.py
@@ -8,10 +8,10 @@ cikLinMarkDown = open("../CikLinBekIn.md", "r", encoding='utf8').read()
 cikLinHtml = markdown2.markdown(cikLinMarkDown)
 
 # CikLinBekIn.html
-f = open('./template/ciklinbeikin.html.jinja2','r',encoding='utf-8')
+f = open('./template/ciklinbekin.html.jinja2','r',encoding='utf-8')
 t = Template(f.read())
 f.close()
 output = t.render(body = cikLinHtml)
-f2 = open('../../build/CikLinBekIn.html',"w", encoding='utf8')
+f2 = open('../build/CikLinBekIn.html',"w", encoding='utf8')
 f2.write(output)
 f2.close()
