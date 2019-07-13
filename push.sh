2c11b76d0943676d2c9cd9281adaa155c70676e8
Radium <ztl8702@users.noreply.github.com>
2019-07-14 06:00:55 +1000

    Setup Travis CI (#20)

---
+++ push.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI (on behalf of @ztl8702)"
+}
+
+prepare() {
+  cp -rf ./build /tmp/
+}
+
+checkout() {
+  git checkout gh-pages
+  git pull
+}
+
+checkout_pr() {
+  git checkout gh-pages
+  git pull
+  git checkout -b gh-pages-review
+}
+
+commit_website_files() {
+  git rm -r '*'
+  rm -rf ./*
+  cp -rf /tmp/build/* ./
+  git add .
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+auth() {
+  git remote add origin-ci https://${GH_TOKEN}@github.com/only3km/ciklinbekin.git > /dev/null 2>&1
+}
+
+upload_files() {
+  git push --quiet --set-upstream origin-ci gh-pages
+}
+
+upload_files_pr() {
+  git push --quiet --set-upstream origin-ci --force gh-pages-review
+}
+
+setup_git
+auth
+prepare
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+  checkout
+else
+  checkout_pr
+fi
+commit_website_files
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+  upload_files
+else
+  if [ "$TRAVIS_SECURE_ENV_VARS" = "true" ]; then
+    upload_files_pr
+  fi
+fi
