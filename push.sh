661ab1c1a78435c49adf853ddc8453c98d8bc7c1
Radium Zheng <ztl8702@msn.com>
2019-07-14 05:34:06 +1000

    tee

--- push.sh
+++ push.sh
@@ -4,20 +4,53 @@ setup_git() {
   git config --global user.name "Travis CI (on behalf of @ztl8702)"
 }
 
-commit_website_files() {
+prepare() {
   cp -rf ./build /tmp/
-  git checkout -b gh-pages-test
-  rm -rf ./*
+}
+
+checkout() {
+  git checkout gh-pages
+}
+
+checkout_pr() {
+  git checkout gh-pages
+  git checkout -b gh-pages-review
+}
+
+commit_website_files() {
+  git rm -r *
   cp -rf /tmp/build/* ./
   git add .
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
+auth() {
+  git remote add origin-ci https://${GH_TOKEN}@github.com/only3km/ciklinbekin.git > /dev/null 2>&1
+}
+
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/only3km/ciklinbekin.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages gh-pages-test
+  git push --quiet --set-upstream origin-ci gh-pages
+}
+
+upload_files_pr() {
+  git push --quiet --set-upstream origin-ci gh-pages-review
 }
 
 setup_git
+auth
+prepare
+if [ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ] then
+  checkout
+else
+  checkout_pr
+fi
 commit_website_files
-upload_files
+if [ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ] then
+  upload_files
+else
+  if [ $TRAVIS_SECURE_ENV_VARS == "true" ] then
+    upload_files_pr
+  fi
+fi
+
+
